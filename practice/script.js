28965035c0c4974e14a1b78642f98060446c56dc
Muhammad Zeeshan Muneer <muneerzeeshan670@gmail.com>
2023-02-07 19:56:42 -0800

    Urgent Push

---
+++ practice/script.js
@@ -0,0 +1,19 @@
+"use strict";
+
+const airline = "Tap Air Portugal";
+const plane = "A320";
+
+
+console.log(plane[0]);
+console.log(plane[1]);
+console.log(plane[2]);
+console.log('B737'[0]);
+
+console.log(airline.length);
+console.log('B373'.length);
+
+console.log(airline.indexOf('r'));
+console.log(airline.lastIndexOf('r'));
+console.log(airline.indexOf("Portugal"));
+
+console.log(airline.slice(4))
